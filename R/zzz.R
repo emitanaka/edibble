b4524fc2da801a317fcf7880c0c0ac95fb4b3c2d
Emi Tanaka <dr.emi.tanaka@gmail.com>
2020-12-31 12:34:57 +1100

    s3_register cannot be found error, fix later

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,5 @@
 .onLoad <- function(...) {
   #s3_register("pillar::pillar_shaft", "vctrs_vctr")
   #s3_register("tibble::type_sum", "vctrs_vctr")
-  s3_register("ggplot2::autoplot", "edbl")
+  #s3_register("ggplot2::autoplot", "edbl")
 }
