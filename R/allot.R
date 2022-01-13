a3e05612d1efadf989c446091828a357bf12368a
Emi <dr.emi.tanaka@gmail.com>
2022-01-13 15:58:04 +1100

    allot_table

---
+++ R/allot.R
@@ -0,0 +1,64 @@
+#' Define the possible allocation of treatments to units
+#'
+#' @description
+#' This function adds the edges between variable nodes to
+#' specify the mapping of units to treatment. This function
+#' does not actually assign specific treatment levels onto actual units.
+#'
+#' @param .data An `edbl_graph` object.
+#' @param ... One-sided or two-sided formula. If the input is a one-sided formula
+#' then the whole treatment is applied to the specified unit.
+#' @family user-facing functions
+#' @examples
+#' start_design() %>%
+#'   set_units(block = 10,
+#'             plot = nested_in(block, 3)) %>%
+#'   set_trts(treat = c("A", "B", "C"),
+#'            pest = c("a", "b")) %>%
+#'   allot_trts(treat ~ plot,
+#'                 pest ~ block)
+#'
+#'
+#' @export
+allot_trts <- function(design, ...) {
+
+  not_edibble(design)
+
+  dots <- list2(...)
+  design$allotment <- dots
+  for(ialloc in seq_along(dots)) {
+    trts <- all.vars(f_lhs(dots[[ialloc]]))
+    # there should be only one unit
+    unit <- all.vars(f_rhs(dots[[ialloc]]))
+    check_var_exists(design, unit, "edbl_unit")
+    uid <- fct_id(design, unit)
+    if(length(trts)) {
+      check_var_exists(design, trts, "edbl_trt")
+      tids <- fct_id(design, trts)
+    } else {
+      check_trt_exists(design)
+      classes <- fct_class(design)
+      tids <- trt_ids(design)
+    }
+
+    design$graph$edges <- add_row(design$graph$edges,
+                                  from = tids, to = uid, alloc = ialloc)
+  }
+  design
+}
+
+#' @export
+allocate_trts <- function(design, ...) {
+  warn("`allocate_trts` is deprecated. Please use `allot_trts` instead.")
+  allot_trts(design, ...)
+}
+
+#' A shorthand for allot, assign and serve
+#'
+#' @export
+allot_table <- function(.design, ..., .order = "random", .seed = NULL, .constrain = nesting(.design)) {
+  .design %>%
+    allot_trts(...) %>%
+    assign_trts(.order = .order, .seed = .seed, .constrain = .constrain) %>%
+    serve_table()
+}
--- R/trts.R
+++ R/trts.R
@@ -26,67 +26,13 @@ set_trts <- function(.design, ...,
 }
 
 
-#' Define the possible allocation of treatments to units
-#'
-#' @description
-#' This function adds the edges between variable nodes to
-#' specify the mapping of units to treatment. This function
-#' does not actually assign specific treatment levels onto actual units.
-#'
-#' @param .data An `edbl_graph` object.
-#' @param ... One-sided or two-sided formula. If the input is a one-sided formula
-#' then the whole treatment is applied to the specified unit.
-#' @family user-facing functions
-#' @examples
-#' start_design() %>%
-#'   set_units(block = 10,
-#'             plot = nested_in(block, 3)) %>%
-#'   set_trts(treat = c("A", "B", "C"),
-#'            pest = c("a", "b")) %>%
-#'   allot_trts(treat ~ plot,
-#'                 pest ~ block)
-#'
-#'
-#' @export
-allot_trts <- function(design, ...) {
-
-  not_edibble(design)
-
-  dots <- list2(...)
-  design$allotment <- dots
-  for(ialloc in seq_along(dots)) {
-    trts <- all.vars(f_lhs(dots[[ialloc]]))
-    # there should be only one unit
-    unit <- all.vars(f_rhs(dots[[ialloc]]))
-    check_var_exists(design, unit, "edbl_unit")
-    uid <- fct_id(design, unit)
-    if(length(trts)) {
-      check_var_exists(design, trts, "edbl_trt")
-      tids <- fct_id(design, trts)
-    } else {
-      check_trt_exists(design)
-      classes <- fct_class(design)
-      tids <- trt_ids(design)
-    }
-
-    design$graph$edges <- add_row(design$graph$edges,
-                                  from = tids, to = uid, alloc = ialloc)
-  }
-  design
-}
-
-#' @export
-allocate_trts <- function(design, ...) {
-  warn("`allocate_trts` is deprecated. Please use `allot_trts` instead.")
-  allot_trts(design, ...)
-}
 
 #' Assign treatments to units
 #'
 #' This function assigns specific treatment levels to actual units.
 #'
-#' @param design An edibble design which should have units, treatments and allotment defined.
-#' @param order A character vector signifying the apportion of treatments to units.
+#' @param .design An edibble design which should have units, treatments and allotment defined.
+#' @param .order A character vector signifying the apportion of treatments to units.
 #' The value should be either "random", "systematic" or "systematic-random".
 #' "random" allocates the treatment randomly to units based on specified allotment with restrictions
 #' implied by unit structure.
@@ -94,39 +40,39 @@ allocate_trts <- function(design, ...) {
 #' "systematic-random" allocates the treatment in a systematic order to units but
 #' where it is not possible to divide treatments equally (as the number of units are not divisible
 #' by the number of levels of the treatment factor), then the extras are chosen randomly.
-#' @param seed A scalar value used to set the seed so that the result is reproducible.
-#' @param constrain The nesting structure for units.
+#' @param .seed A scalar value used to set the seed so that the result is reproducible.
+#' @param .constrain The nesting structure for units.
 #'
 #' @export
-assign_trts <- function(design, order = "random", seed = NULL, constrain = nesting(design)) {
+assign_trts <- function(.design, .order = "random", .seed = NULL, .constrain = nesting(.design)) {
 
-  not_edibble(design)
+  not_edibble(.design)
 
-  set.seed(seed)
-  design %@% "seed" <- seed
-  design %@% "seeds" <- .Random.seed
+  set.seed(.seed)
+  .design %@% "seed" <- .seed
+  .design %@% "seeds" <- .Random.seed
 
-  for(ialloc in seq_along(design$allotment)) {
-    trts <- all.vars(f_lhs(design$allotment[[ialloc]]))
+  for(ialloc in seq_along(.design$allotment)) {
+    trts <- all.vars(f_lhs(.design$allotment[[ialloc]]))
     # there should be only one unit
-    unit <- all.vars(f_rhs(design$allotment[[ialloc]]))
-    uid <- fct_id(design, unit)
+    unit <- all.vars(f_rhs(.design$allotment[[ialloc]]))
+    uid <- fct_id(.design, unit)
     if(length(trts)) {
-      tids <- fct_id(design, trts)
+      tids <- fct_id(.design, trts)
     } else {
-      classes <-fct_class(design)
-      tids <- trt_ids(design)
+      classes <-fct_class(.design)
+      tids <- trt_ids(.design)
     }
 
-    luids <- lvl_nodes_filter(design, idvar == uid)$id
-    tdf <- lvl_nodes_filter(design, idvar %in% tids)
-    tidf <- expand.grid(split(tdf$id, fct_label(design, tdf$idvar)))
+    luids <- lvl_nodes_filter(.design, idvar == uid)$id
+    tdf <- lvl_nodes_filter(.design, idvar %in% tids)
+    tidf <- expand.grid(split(tdf$id, fct_label(.design, tdf$idvar)))
     ntrts <- nrow(tidf)
-    permutation <- switch(order,
+    permutation <- switch(.order,
                           "systematic" = rep(1:nrow(tidf), length.out = length(luids)),
                           "systematic-random" = rep(sample(nrow(tidf)), length.out = length(luids)),
                           "random" = {
-                            if(is_empty(constrain[[unit]])) {
+                            if(is_empty(.constrain[[unit]])) {
                               sample(rep(sample(nrow(tidf)), length.out = length(luids)))
                             } else {
 
@@ -134,10 +80,10 @@ assign_trts <- function(design, order = "random", seed = NULL, constrain = nesti
                               # based on `constrain`
 
                               # find the grandest ancestor
-                              vanc <- fct_ancestor(design, id = uid)
-                              vanc <- vanc[vanc %in% unit_ids(design)]
-                              udf <- as.data.frame(serve_units(select_units(design, !!fct_label(design, vanc))))
-                              gparent <- fct_label(design, vanc[2])
+                              vanc <- fct_ancestor(.design, id = uid)
+                              vanc <- vanc[vanc %in% unit_ids(.design)]
+                              udf <- as.data.frame(serve_units(select_units(.design, !!fct_label(.design, vanc))))
+                              gparent <- fct_label(.design, vanc[2])
                               blocksizes <- as.data.frame(table(table(udf[[gparent]])))
                               blocksizes$size <- as.numeric(as.character(blocksizes$Var1))
                               for(isize in seq(nrow(blocksizes))) {
@@ -165,21 +111,21 @@ assign_trts <- function(design, order = "random", seed = NULL, constrain = nesti
                               out
                             }
                           },
-                          abort(paste("The", order, "`order` is not implemented.")))
+                          abort(paste("The", .order, "`.order` is not implemented.")))
 
     tout <- tidf[permutation, , drop = FALSE]
 
 
     for(itvar in seq_along(tout)) {
-      design$graph$levels$edges <- add_row(design$graph$levels$edges,
+      .design$graph$levels$edges <- add_row(.design$graph$levels$edges,
                                      from = tout[[itvar]],
                                      to = luids, alloc = ialloc)
     }
   }
 
-  design$assignment <- order
+  .design$assignment <- .order
 
-  design
+  .design
 }
 
 #' @export
