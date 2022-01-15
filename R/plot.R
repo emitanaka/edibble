ebc113640f99191ed83fc36c8c6b6c9cba72b47b
Emi <dr.emi.tanaka@gmail.com>
2022-01-15 15:56:01 +1100

    improved plot

--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,5 @@
 #' @export
-plot.edbl_design <- function(.design, view = c("factors", "levels"), width = "100%", height = NULL, ...) {
+plot.edbl_design <- function(.design, view = c("factors", "levels"), width = "100%", height = NULL, seed = 1, ...) {
   if(!require("visNetwork")) abort("You need to install `visNetwork` package.")
   view <- match.arg(view)
   nodes <- switch(view,
@@ -8,6 +8,10 @@ plot.edbl_design <- function(.design, view = c("factors", "levels"), width = "10
   nodes$group <- switch(view,
                         "factors" = gsub("edbl_", "", nodes$class),
                         "levels" = nodes$var)
+  class2shape <- c("edbl_unit" = "circle",
+                   "edbl_trt" = "diamond",
+                   "edbl_rcrd" = "database")
+  nodes$shape <- class2shape[fct_class(.design, nodes$idvar)]
   edges <- switch(view,
                   "factors" = fct_edges(.design),
                   "levels" = lvl_edges(.design))
@@ -16,10 +20,15 @@ plot.edbl_design <- function(.design, view = c("factors", "levels"), width = "10
                          width = width,
                          height = height,
                          main = .design$name) %>%
+    visNetwork::visLayout(randomSeed = seed) %>%
+    visNetwork::visNodes(id = "id") %>%
     visNetwork::visEdges(arrows = "to", id = "id") %>%
-    visNetwork::visLegend() %>%
-    visNetwork::visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE,
-                           collapse = TRUE) %>%
+    #visNetwork::visLegend() %>%
+    visNetwork::visOptions(highlightNearest = TRUE,
+                           nodesIdSelection = TRUE,
+                           collapse = TRUE,
+                           manipulation = TRUE,
+                           selectedBy = "group") %>%
     visNetwork::visInteraction(navigationButtons = TRUE)
 }
 
