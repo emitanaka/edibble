907e4065011f1857c30085b54f70f8fe979180f0
Emi <dr.emi.tanaka@gmail.com>
2022-01-17 09:58:11 +1100

    update latin_rectangle so it's a collection of latin squares instead

--- R/latin.R
+++ R/latin.R
@@ -1,42 +1,51 @@
-latin_square <- function(n, random = TRUE) {
-  general_latin_rectangle(n, n, n, random = random)
+latin_square <- function(n, randomise = TRUE) {
+  out <- matrix(nrow = n, ncol = n)
+  out[1,] <- if(randomise) sample(1:n) else 1:n
+  for(i in 2:n) {
+    out[i,] <- lag_vector(out[i - 1, ])
+  }
+  if(randomise) {
+    out <- out[, sample(1:n)]
+    out <- out[sample(1:n), ]
+  }
+  out
 }
 
-latin_rectangle <- function(nr, nc, nt, random = TRUE) {
-  r <- ceiling(nc/nt)
-  out <- matrix(nrow = nr, ncol = r * nt)
-  out[1, ] <- if(random) as.vector(replicate(r, sample(1:nt))) else as.vector(replicate(r, 1:nt))
-  for(i in 2:nr) {
-    out[i, ] <- lag_vector(out[i - 1, ])
-  }
-  out <- out[, 1:nc]
+latin_rectangle <- function(nr, nc, nt, randomise = TRUE) {
+  r1 <- ceiling(nr/nt)
+  r2 <- ceiling(nc/nt)
+  out <- do.call("rbind", lapply(1:r1, function(i) {
+    do.call("cbind",
+      lapply(1:r2, function(j) {
+        latin_square(nt, randomise = randomise)
+      }
+    ))
+  }))
+
+  out <- out[1:nr, 1:nc]
+
   res1 <- apply(out, 1, function(x) table(factor(x, 1:nt)))
   res2 <- apply(out, 2, function(x) table(factor(x, 1:nt)))
   diff1 <- apply(res1, 2, function(x) max(x) - min(x))
   diff2 <- apply(res2, 2, function(x) max(x) - min(x))
 
-  if(random && (any(diff1 > 1) | any(diff2 > 1))) return(latin_rectangle(nr, nc, nt, random))
-  if(random) {
-    out <- out[sample(1:nr), ]
-    cindex <- as.vector(replicate(r, sample(1:nt)) + nt * matrix(0:(r - 1), ncol = r, nrow = nt, byrow = TRUE))
-    cindex <- intersect(cindex, 1:nc)
-    out <- out[, cindex]
-  }
+  if(randomise && (any(diff1 > 1) | any(diff2 > 1))) return(latin_rectangle(nr, nc, nt, randomise))
+
   out
 }
 
-latin_array <- function(dim, nt, random = TRUE) {
+latin_array <- function(dim, nt, randomise = TRUE) {
   ndim <- length(dim)
-  if(ndim == 2) return(latin_rectangle(dim[1], dim[2], nt, random))
+  if(ndim == 2) return(latin_rectangle(dim[1], dim[2], nt, randomise))
   r <- ceiling(dim[ndim]/nt)
   dim_ext <- c(dim[-ndim], r * nt)
   out <- array(dim = dim_ext)
   #browser()
-  out <- assign_array(out, 1, 1, latin_array(dim_ext[-1], nt, random))
+  out <- assign_array(out, 1, 1, latin_array(dim_ext[-1], nt, randomise))
   for(i in 2:dim[1]) {
     out <- assign_array(out, 1, i, lag_array(index_array(out, 1, i - 1), 1))
   }
-  if(random) {
+  if(randomise) {
     for(i in seq_along(dim[-ndim])) {
       out <- sample_array(out, i)
     }
