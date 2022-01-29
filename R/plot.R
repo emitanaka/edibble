e908b363bbf86be7c71a2737909f075d26ab0faa
Emi <dr.emi.tanaka@gmail.com>
2022-01-29 18:35:01 +1100

    view -> which (for selecting fct or lvl), view -> string for interactivity control

--- R/plot.R
+++ R/plot.R
@@ -1,18 +1,21 @@
 
 #' Interactive plot of the edibble design
 #' @param .design An edibble design.
-#' @param view A string of either "factors" or "levels".
+#' @param which A string of either "factors" or "levels".
 #' @param width,height The width and height of the plot.
 #' @param seed A seed number so same plot is always generated.
-#' @param show_buttons A logical value indicating whether to hide or show the buttons.
+#' @param view A string of either "show-buttons" (default), "hide-buttons", "static"
 #' @export
-plot.edbl_design <- function(.design, view = c("factors", "levels"), width = "100%", height = NULL, seed = 1, show_buttons = TRUE, ...) {
+plot.edbl_design <- function(.design, which = c("factors", "levels"), width = "100%", height = NULL, seed = 1,
+                             view = c("show-buttons", "hide-buttons", "static"), ...) {
   if(!require("visNetwork")) abort("You need to install `visNetwork` package.")
+  which <- match.arg(which)
   view <- match.arg(view)
-  nodes <- switch(view,
+
+  nodes <- switch(which,
                   "factors" = fct_nodes(.design),
                   "levels" = lvl_nodes(.design))
-  nodes$group <- switch(view,
+  nodes$group <- switch(which,
                         "factors" = gsub("edbl_", "", nodes$class),
                         "levels" = nodes$var)
   nodes$label <- nodes$name
@@ -20,7 +23,7 @@ plot.edbl_design <- function(.design, view = c("factors", "levels"), width = "10
                    "edbl_trt" = "diamond",
                    "edbl_rcrd" = "database")
   nodes$shape <- class2shape[fct_class(.design, nodes$idvar)]
-  edges <- switch(view,
+  edges <- switch(which,
                   "factors" = fct_edges(.design),
                   "levels" = lvl_edges(.design))
   out <- visNetwork::visNetwork(nodes = nodes,
@@ -32,17 +35,25 @@ plot.edbl_design <- function(.design, view = c("factors", "levels"), width = "10
     visNetwork::visNodes(id = "id") %>%
     visNetwork::visEdges(arrows = "to", id = "id")
     #visNetwork::visLegend() %>%
-  if(show_buttons) {
-    out %>%
-      visNetwork::visOptions(highlightNearest = TRUE,
-                             nodesIdSelection = TRUE,
-                             collapse = TRUE,
-                             manipulation = TRUE,
-                             selectedBy = "group") %>%
-      visNetwork::visInteraction(navigationButtons = TRUE)
-  } else {
-    out
-  }
+  switch(view,
+         "show-buttons" = out %>%
+           visNetwork::visOptions(highlightNearest = TRUE,
+                                  nodesIdSelection = TRUE,
+                                  collapse = TRUE,
+                                  manipulation = TRUE,
+                                  selectedBy = "group") %>%
+           visNetwork::visInteraction(navigationButtons = TRUE),
+         "hide-buttons" = out,
+         "static" = out %>%
+           visNetwork::visInteraction(dragNodes = FALSE,
+                                      dragView = FALSE,
+                                      hover = FALSE,
+                                      keyboard = FALSE,
+                                      multiselect = FALSE,
+                                      navigationButtons = FALSE,
+                                      selectable = FALSE,
+                                      zoomView = FALSE) )
+
 }
 
 #' @rdname plot.edbl_design
