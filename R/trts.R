d776103fe07df65a6354fd81a5898314c740d4ae
Emi <dr.emi.tanaka@gmail.com>
2022-01-31 14:31:04 +1100

    use `AlgDesign` for optimal block designs

--- R/trts.R
+++ R/trts.R
@@ -93,7 +93,8 @@ assign_trts <- function(.design, order = "random", seed = NULL, constrain = nest
                               vparents <- setdiff(vparents, uid)
 
                               if(length(vparents)==1L) {
-                                permute_parent_one(.design, vparents, udf, ntrts)
+                                #permute_parent_one(.design, vparents, udf, ntrts)
+                                permute_parent_one_alg(.design, vparents, udf, ntrts)
                               } else {
                                 permute_parent_more_than_one(.design, vparents, udf, ntrts)
                               }
@@ -133,7 +134,14 @@ permute_parent_more_than_one <- function(.design, vids, udf, ntrts) {
 
 }
 
-
+permute_parent_one_alg <- function(.design, vid, udf, ntrts) {
+  gparent <- fct_names(.design, vid)
+  blocksizes <- table(udf[[gparent]])
+  res <- AlgDesign::optBlock(~.,
+                      withinData = data.frame(tindex = factor(1:ntrts)),
+                      blocksizes = blocksizes)
+  unname(unlist(lapply(res$Blocks, function(x) sample(as.integer(x$tindex)))))
+}
 
 
 permute_parent_one <- function(.design, vid, udf, ntrts) {
