d31ef3bc949e29f5b446a0d78fb1c1523c6cacaf
Emi Tanaka <emi.tanaka@monash.edu>
2022-02-06 15:12:42 +1100

    traits -> attrs & use crossed_by insteaed of formula

---
+++ R/cross.R
@@ -0,0 +1,20 @@
+
+#' Specify the units to cross to index a new unit
+#'
+#' `crossed_by(A, B)` is the same as `~A:B` but `crossed_by` offers more control over the names of the new units
+#' as well as adding new attributes.
+#'
+#' @param ... a sequence of units
+#' @param prefix Currently not implemented.The prefix of the label.
+#' @param suffix Currently not implemented.The suffix of the label.
+#' @param leading0 Currently not implemented.Whether there should be a leading 0 if labels are made.
+#' @param sep Currently not implemented.A separator added between prefix and the number if prefix is empty.
+#' @param attrs Currently not implemented.
+#' @export
+crossed_by <- function(..., prefix = NULL, suffix = NULL, leading0 = NULL, sep = NULL, attrs = NULL) {
+  e <- exprs(...)
+  structure(as.formula(paste0("~", paste0(map_chr(e, as.character), collapse = ":"))),
+            attrs = attrs)
+}
+
+
--- R/nest.R
+++ R/nest.R
@@ -10,13 +10,14 @@
 #' @param distinct whether the child variables are distinct in the levels that it is nesting
 #' @param leading0 Whether there should be a leading 0 if labels are made.
 #' @param sep A separator added between prefix and the number if prefix is empty.
-#' @param traits A named vector where names and values correspond to attribute names and values of the variable.
+#' @param attrs A named vector where names and values correspond to attribute names and values of the variable, or
+#'   a data frame.
 #' @seealso See [set_units()] for examples of how to use this.
 #' @export
 nested_in <- function(x, ..., prefix = "", suffix = "",
                       distinct = TRUE, leading0 = FALSE,
                       sep = edibble_labels_opt("sep"),
-                      traits = NULL) {
+                      attrs = NULL) {
   top <- caller_env()$.top_env
   if(is.null(top$.fname)) abort("The `nested_in` function must be used within `set_units` function.")
   des <- top$des
@@ -57,8 +58,8 @@ nested_in <- function(x, ..., prefix = "", suffix = "",
                                  leading0 = leading0,
                                  compact = FALSE,
                                  keyname = parent_name)
-  ltraits <- as.list(traits)
-  attributes(child_levels) <- c(ltraits, attributes(child_levels),
+  lattrs <- as.list(attrs)
+  attributes(child_levels) <- c(lattrs, attributes(child_levels),
                                 list(parents = attr(args, "parents")))
   class(child_levels) <- c("nst_levels", class(child_levels))
   return(child_levels)
