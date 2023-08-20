b76ff88caf95f6d15b05349fb5a8bf7607faa728
Your Name <dr.emi.tanaka@gmail.com>
2023-08-20 13:08:31 -0400

    update the external fct_nodes, fct_edges, lvl_nodes, lvl_edges commands

--- R/graph.R
+++ R/graph.R
@@ -37,33 +37,55 @@ NULL
 
 #' @rdname design_data
 #' @export
-fct_nodes <- function(edibble) {
-  prov <- activate_provenance(edibble)
-  prov$fct_nodes
+fct_nodes <- function(x) {
+  prov <- get_provenance(x)
+  fnodes <- prov$fct_nodes
+  fnodes[, c("name", "role", "attrs")]
 }
 
 #' @rdname design_data
 #' @export
-fct_edges <- function(edibble) {
-  prov <- activate_provenance(edibble)
-  prov$fct_edges
+fct_edges <- function(x) {
+  prov <- get_provenance(x)
+  fedges <- prov$fct_edges
+  fedges[, c("var_from", "var_to", "type", "group", "attrs")]
 }
-
 
 
 #' @rdname design_data
 #' @export
-lvl_nodes <- function(edibble) {
-  prov <- activate_provenance(edibble)
-  prov$lvl_nodes
+lvl_nodes <- function(x) {
+  prov <- get_provenance(x)
+  lnodes <- prov$lvl_nodes
+  lnodes <- lapply(lnodes, function(x) x[, "value", drop = FALSE])
+  names(lnodes) <- prov$fct_names(id = as.numeric(names(lnodes)))
+  lnodes
 }
 
 #' @rdname design_data
 #' @export
-lvl_edges <- function(edibble) {
-  prov <- activate_provenance(edibble)
-  prov$lvl_edges
+lvl_edges <- function(x) {
+  prov <- get_provenance(x)
+  ledges <- prov$lvl_edges
+  lnodes <- prov$lvl_nodes
+  lnodes_df <- do.call("rbind", lapply(names(lnodes), function(x) data.frame(fid = as.numeric(x), id = lnodes[[x]]$id)))
+  ledges$var_from <- prov$fct_names(id = lnodes_df[match(ledges$from, lnodes_df$id), "fid"])
+  ledges$var_to <- prov$fct_names(id = lnodes_df[match(ledges$to, lnodes_df$id), "fid"])
+  ledges <- split(ledges, paste(ledges$var_from, "->", ledges$var_to))
+  lapply(ledges, function(df) {
+    df$val_from <- prov$lvl_values(id = df$from, fid = prov$fct_id(name = df$var_from[1]))
+    df$val_to <- prov$lvl_values(id = df$to, fid = prov$fct_id(name = df$var_to[1]))
+    df[, c("var_from", "var_to", "val_from", "val_to", "attrs")]
+  })
 }
 
 
+get_provenance <- function(x) {
+  if(is_provenance(x)) {
+    x
+  } else {
+    not_edibble(x)
+    activate_provenance(x)
+  }
+}
 
