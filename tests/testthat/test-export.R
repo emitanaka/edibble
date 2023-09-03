e32e4c11e90f5d77944b3e5316346dec29fe6b5a
Emi Tanaka <dr.emi.tanaka@gmail.com>
2023-09-03 17:59:14 +1000

    test for export include author now

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -2,7 +2,7 @@ test_that("export with no record", {
   set.seed(1)
   des <- suppressMessages(takeout())
   fn <- tempfile()
-  suppressMessages(export_design(des, file = fn, overwrite = TRUE))
+  suppressMessages(export_design(des, file = fn, overwrite = TRUE, author = "Emi Tanaka"))
   dat <- openxlsx2::read_xlsx(fn, sheet = 2)
 
   expect_equal(dat, as_tibble(des), ignore_attr = TRUE)
