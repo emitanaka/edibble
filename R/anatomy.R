a4dc941e28dfc505f55fe6c43609923b7d84dcc1
Emi Tanaka <dr.emi.tanaka@gmail.com>
2023-09-03 14:10:10 +1000

    fix anatomy

--- R/anatomy.R
+++ R/anatomy.R
@@ -16,7 +16,7 @@ anatomy <- function(.edibble, ...) {
   des <- edbl_design(.edibble)
   tab <- edbl_table(.edibble)
   prov <- activate_provenance(des)
-  trt_str <- stats::as.formula(paste0("~", paste0(prov$trt_names, collapse = "*")))
+  trt_str <- stats::as.formula(paste0("~", paste0(prov$trt_names(), collapse = "*")))
   out <- dae::designAnatomy(list(unit = des$anatomy, trt = trt_str), data = tab, ...)
   structure(out,
             class = c("des_anatomy", class(out)))
@@ -31,13 +31,13 @@ print.des_anatomy <- function(x, ...) {
 add_anatomy <- function(anatomy, fresh, name, role) {
   if(role=="edbl_unit") {
     if(is.null(anatomy)) {
-      anatomy <- as.formula(paste0("~", name))
+      anatomy <- stats::as.formula(paste0("~", name))
     } else {
       term <- ifelse(inherits(fresh, "nest_lvls"),
                      paste0(attr(fresh, "keyname"), "/", name),
                      name)
-      anatomy <- update(anatomy,
-                             as.formula(paste0("~ . + ", term)), evaluate = FALSE)
+      anatomy <- stats::update(anatomy,
+                             stats::as.formula(paste0("~ . + ", term)), evaluate = FALSE)
     }
   }
   anatomy
